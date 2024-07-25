fca5a06d6bed875719011debc49e39d316339b7f
matteenm <matteenm@users.noreply.github.com>
2024-07-25 16:59:43 +0000

    fix(lint): auto-fix [ci]

--- src/utils/intervalUpdates.ts
+++ src/utils/intervalUpdates.ts
@@ -127,10 +127,7 @@ export function updateTokenDayData(token: Token, event: ethereum.Event): TokenDa
   const timestamp = event.block.timestamp.toI32()
   const dayID = timestamp / 86400
   const dayStartTimestamp = dayID * 86400
-  const tokenDayID = token.id
-    .toString()
-    .concat('-')
-    .concat(dayID.toString())
+  const tokenDayID = token.id.toString().concat('-').concat(dayID.toString())
   const tokenPrice = token.derivedETH.times(bundle.ethPriceUSD)
 
   let tokenDayData = TokenDayData.load(tokenDayID)
@@ -170,10 +167,7 @@ export function updateTokenHourData(token: Token, event: ethereum.Event): TokenH
   const timestamp = event.block.timestamp.toI32()
   const hourIndex = timestamp / 3600 // get unique hour within unix history
   const hourStartUnix = hourIndex * 3600 // want the rounded effect
-  const tokenHourID = token.id
-    .toString()
-    .concat('-')
-    .concat(hourIndex.toString())
+  const tokenHourID = token.id.toString().concat('-').concat(hourIndex.toString())
   let tokenHourData = TokenHourData.load(tokenHourID)
   const tokenPrice = token.derivedETH.times(bundle.ethPriceUSD)
 
