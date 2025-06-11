d0ffd5cd3307605c0881e4379d829fdc82cab7f9
matteen <matteen.mobasheran@uniswap.org>
2025-06-11 17:30:10 -0400

    Tracking for Euler Hooks (#33)
    * euler unichain tracking
    
    * mainnet
    
    * remove graft to make yaml more generic

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,3 +1,4 @@
+import { handleHookDeployed, handleHookUninstalled } from './euler'
 import { handleModifyLiquidity } from './modifyLiquidity'
 import { handleInitialize } from './poolManager'
 import { handleSubscription } from './subscribe'
@@ -5,4 +6,13 @@ import { handleSwap } from './swap'
 import { handleTransfer } from './transfer'
 import { handleUnsubscription } from './unsubscribe'
 
-export { handleInitialize, handleModifyLiquidity, handleSubscription, handleSwap, handleTransfer, handleUnsubscription }
+export {
+  handleHookDeployed,
+  handleHookUninstalled,
+  handleInitialize,
+  handleModifyLiquidity,
+  handleSubscription,
+  handleSwap,
+  handleTransfer,
+  handleUnsubscription,
+}
